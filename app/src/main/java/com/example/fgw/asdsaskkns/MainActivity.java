996d34c429e9e705e27ac1f89400ce24594a9449
shivumgrover <shivumgrover99@gmail.com>
2018-08-08 14:49:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/fgw/asdsaskkns/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.fgw.asdsaskkns;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(6000);
+        animationDrawable.setExitFadeDuration(2000);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
+    }
+}
