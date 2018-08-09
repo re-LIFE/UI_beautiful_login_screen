a4c722bba123f6535364adf755fe16b23c734490
shivumgrover <shivumgrover99@gmail.com>
2018-08-09 05:11:53 -0700

    updated the button functionality will add firebase functionality to the project soon

--- app/src/main/java/com/example/fgw/asdsaskkns/MainActivity.java
+++ app/src/main/java/com/example/fgw/asdsaskkns/MainActivity.java
@@ -4,14 +4,25 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
     AnimationDrawable animationDrawable;
     RelativeLayout relativeLayout;
+    ImageView imageView;
+    EditText inputE,P;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +32,42 @@ public class MainActivity extends AppCompatActivity {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
+        final LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
 
         relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
         animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
         animationDrawable.setEnterFadeDuration(6000);
         animationDrawable.setExitFadeDuration(2000);
+
+        imageView=findViewById(R.id.imageView2);
+        progressBar= findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        inputE = findViewById(R.id.editText);
+        P = findViewById(R.id.editText2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aa();
+            }
+        });
+
+    }
+
+    private void aa() {
+        String email = inputE.getText().toString();
+        final String password = P.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
 
     }
 
